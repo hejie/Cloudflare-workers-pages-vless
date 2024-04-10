47753b0b4686f6f6f378738cc3d1bf64c3cefa81
JIE <hj7.com@gmail.com>
2024-04-10 21:52:35 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -54,7 +54,7 @@ export default {
 						return new Response(`${vlessConfig}`, {
 							status: 200,
 							headers: {
-								"Content-Type": "text/html; charset=utf-8",
+								"Content-Type": "text/plain; charset=utf-8",
 							}
 						});
 					};
